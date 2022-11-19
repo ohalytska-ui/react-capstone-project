de7c54764217fb1f316703f03f21eb98c30ec9e1
ohalytska-ui <77673057+ohalytska-ui@users.noreply.github.com>
2022-11-19 13:58:30 +0100

    feat: added setup to deploy app

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import compression from 'compression';
-// import path from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import db from './db/db.js';
 
 // server port
 const port = 5000 || process.env.PORT;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const buildPath = path.join(__dirname, '../twitter-clone/build');
 
 // view engine setup
 dotenv.config();
@@ -15,9 +18,9 @@ const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.use(compression()); // compress all routes
+app.use(express.static(buildPath));
 
 app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname, '../my-app/build', 'index.html'));
   res.json({ message: 'alive' });
 });
 
