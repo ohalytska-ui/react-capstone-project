1549823ccac87661aed5e8f2b398625e17470252
ohalytska-ui <77673057+ohalytska-ui@users.noreply.github.com>
2022-11-20 02:40:12 +0100

    fix: fixed small issues

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 
 import 'antd/dist/reset.css';
-import { Navigate, Route, Routes, MemoryRouter } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Login, Signup, FeedTwitter } from './components';
 import { ConfigProvider } from 'antd';
 
@@ -9,12 +9,10 @@ const App: FC = () => {
   return (
     <ConfigProvider>
       <Routes>
-        <MemoryRouter>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/users" element={<FeedTwitter />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </MemoryRouter>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/users" element={<FeedTwitter />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </ConfigProvider>
   );
